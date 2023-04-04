e566fba286979c5198118d4c7eac30c025fbdd9d
Alexandr Golub <it.quality.analyst@gmail.com>
2023-04-04 21:00:44 +0300

    Done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
+// Import gallery items
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+// Select gallery container element and create gallery HTML
+const galleryContainerEl = document.querySelector('.gallery');
+const galleryHTML = createGalleryHTML(galleryItems);
+galleryContainerEl.insertAdjacentHTML('beforeend', galleryHTML);
+
+// Function to create gallery HTML from items
+function createGalleryHTML(items) {
+	return items.map(({ preview, original, description }) => {
+   return `
+      <div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+         <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+         </a>
+      </div>
+   `;
+	}).join('');
+}
+
+// Function to handle container click event
+const onContainerClick = (e) => {
+	e.preventDefault();
+
+  // Get the source URL of the clicked image
+const source = e.target.dataset.source;
+   if (!source) return;
+
+  // Create and show a lightbox with the clicked image
+const instance = basicLightbox.create(`
+   <img src="${source}" width="800" height="600">
+   `);
+   instance.show();
+};
+
+// Add click event listener to gallery container
+galleryContainerEl.addEventListener('click', onContainerClick);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
+// Import gallery items from external file
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+// Select the gallery container and create the gallery HTML markup
+const galleryContainerEl = document.querySelector('.gallery');
+const imagesEl = createGalleryImg(galleryItems);
+galleryContainerEl.insertAdjacentHTML('beforeend', imagesEl);
+
+// Function to create the gallery HTML markup
+function createGalleryImg(items) {
+// Map through each gallery item and create HTML markup for each one
+	return items.map(({ preview, original, description }) => {
+   	return `
+   		<ul class="gallery">
+      		<li>
+         		<a class="gallery__item" href="${original}">
+            		<img class="gallery__image" src="${preview}" alt="${description}" />
+         		</a>
+      		</li>
+   		</ul>
+		`;
+	}).join('');
+}
+
+// Initialize SimpleLightbox plugin with gallery links
+const lightbox = new SimpleLightbox('.gallery a', {
+	captionsData: "alt",
+	captionDelay: 250,
+	captionType: "alt",
+});
